d135d9f50c3e3796d3431ab92995b3273523d11b
Simon Mehari <simon.mehari@castra.se>
2023-11-28 11:02:17 +0100

    added config file

---
+++ src/Config.js
@@ -0,0 +1,34 @@
+export default {
+    "version": "0.0.1",
+    "config": {
+        "features": {
+            "accessibility": {
+                "contrast": true,
+                "font_size": true,
+                "spacing": true,
+                "cursor": true,
+                "highlight": true,
+                "dark_mode": true
+            },
+            "support": {
+                "urls": {
+                    "page":"/support",
+                    "chat":"/support/chat",
+                    "faq":"/support/faq",
+                },
+                "xxx": "yyy"
+            },
+            "languages": ["en", "sv"]
+        }
+    },
+    "style": {
+        "primary_color": "#000000",
+        "dark_mode": false
+    },
+    "context": {
+        "fingerprint": "32053362-7d84-11ee-b962-0242ac120002",
+        "domain": "test.se",
+        "path": "/path/to/fun",
+        "locale": "en"
+    }
+}

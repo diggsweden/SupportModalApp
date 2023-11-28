02fad6bda22098658e6ce227fff883f7abdbb286
Simon Mehari <simon.mehari@castra.se>
2023-11-28 11:13:03 +0100

    adds show attribute to the item's list

--- src/Items.js
+++ src/Items.js
@@ -2,26 +2,27 @@ export default [
     {
         title: 'Support',
         content: [
-            { name: 'Öppna Chatten', icon: 'chat icon' },
-            { name: 'Till Support Sidan', icon: 'support icon' },
-            { name: 'Till Frågor och Svar', icon: 'faq icon' }
+            { name: 'Öppna Chatten', icon: 'chat icon', show: true },
+            { name: 'Till Support Sidan', icon: 'support icon', show: true },
+            { name: 'Till Frågor och Svar', icon: 'faq icon', show: true },
         ]
     },
     {
         title: 'Tillgänglighet',
         content: [
-            { name: 'Ljud', icon: 'sound icon' },
-            { name: 'Monochrom färg', icon: 'color icon' },
-            { name: 'Stor text', icon: 'text icon' },
-            { name: 'High Contrast', icon: 'contrast icon' },
-            { name: 'Text till tal', icon: 'speech icon' }
+            { name: 'Ljud', icon: 'sound icon', show: true },
+            { name: 'Monochrom färg', icon: 'color icon', show: true },
+            { name: 'Stor text', icon: 'text icon', show: true },
+            { name: 'High Contrast', icon: 'contrast icon', show: true },
+            { name: 'Läs upp text', icon: 'speech icon', show: true },
+            { name: 'Text till tal', icon: 'speech icon', show: true }
         ]
     },
     {
         title: 'Change Language',
         content: [
-            { name: 'English', icon: 'english icon' },
-            { name: 'Svenska', icon: 'swedish icon' }
+            { name: 'English', icon: 'english icon' , show: true},
+            { name: 'Svenska', icon: 'swedish icon', show: true },
         ]
     }
 ];
